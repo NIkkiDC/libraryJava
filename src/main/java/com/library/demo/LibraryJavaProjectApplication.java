7a1f51b78e43f8b70a0a1c035f171633b836992f
Dominique <DCWTLT1@gmail.com>
2023-05-08 15:05:06 -0500

    commit

---
+++ src/main/java/com/library/demo/LibraryJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.library.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryJavaProjectApplication.class, args);
+	}
+
+}
