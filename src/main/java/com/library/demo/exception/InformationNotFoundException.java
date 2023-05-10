1f37bd3e7419e195cbe6b3d3b30d99bbfac5cbaf
Dominique <DCWTLT1@gmail.com>
2023-05-09 20:57:02 -0500

    updating the extend runtime for the import org.springframework.http.HttpStatus; import org.springframework.web.bind.annotation.ResponseStatus;
        @ResponseStatus(HttpStatus.NOT_FOUND)
        public class InformationNotFoundException extends RuntimeException {
            public InformationNotFoundException(String message) {
                super(message);
            }
        }

--- src/main/java/com/library/demo/exception/InformationNotFoundException.java
+++ src/main/java/com/library/demo/exception/InformationNotFoundException.java
@@ -1,4 +1,12 @@
 package com.library.demo.exception;
 
-public class InformationNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InformationNotFoundException extends RuntimeException {
+    public InformationNotFoundException(String message) {
+        super(message);
+    }
 }
+
