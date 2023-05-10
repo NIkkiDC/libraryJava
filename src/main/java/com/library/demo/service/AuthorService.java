d5617cdbaa21784618c2b700d32a7e2f620d35c1
Dominique <DCWTLT1@gmail.com>
2023-05-10 14:14:25 -0500

    retrieving an Author object from the database.

--- src/main/java/com/library/demo/service/AuthorService.java
+++ src/main/java/com/library/demo/service/AuthorService.java
@@ -1,4 +1,24 @@
 package com.library.demo.service;
 
+import com.library.demo.model.Author;
+import com.library.demo.repository.AuthorRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
 public class AuthorService {
+
+    private AuthorRepo authorRepo;
+
+    @Autowired
+    public void setAuthorRepo(AuthorRepo authorRepo){
+        this.authorRepo = authorRepo;
+    }
+    public Optional<Author> getAuthorById(Long id){
+        return authorRepo.findById(id);
+    }
+
+
+
 }
