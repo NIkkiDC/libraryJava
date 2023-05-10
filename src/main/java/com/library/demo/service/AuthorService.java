7d0eba7ff6e50befb775de5eb69928e0405552b1
Dominique <DCWTLT1@gmail.com>
2023-05-10 15:46:28 -0500

    updating the author object in the database with a new return that will be illustrated if their is an error of an Authors name not being returned

--- src/main/java/com/library/demo/service/AuthorService.java
+++ src/main/java/com/library/demo/service/AuthorService.java
@@ -1,15 +1,20 @@
 package com.library.demo.service;
 
+import com.library.demo.exception.InformationNotFoundException;
 import com.library.demo.model.Author;
 import com.library.demo.repository.AuthorRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 import java.util.Optional;
 
 public class AuthorService {
 
     private AuthorRepo authorRepo;
+
+    /**
+     *
+     * @param authorRepo
+     */
+
 
     @Autowired
     public void setAuthorRepo(AuthorRepo authorRepo){
@@ -20,6 +25,18 @@ public class AuthorService {
     }
     public Author createAuthor(Author author){
         return authorRepo.save(author);
+    }
+
+    public Optional<Author> updateAuthor(Long authorId, Author authorObject) throws InformationNotFoundException {
+        Optional<Author> author = authorRepo.findById(authorId);
+        if (author.isPresent()){
+            author.get().setName(authorObject.getName());
+            author.get().setDescription(authorObject.getDescription());
+            authorRepo.save(author.get());
+            return author;
+        } else {
+            throw new InformationNotFoundException("Author with id " +authorId + " was not found");
+        }
     }
 
 
